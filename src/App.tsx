4489abbe66889c13e096cb1d735bbc4a857a322d
Aybek <idevkun@gmail.com>
2025-03-15 21:50:57 +0500

    "lecture 8"

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export const App = () => {
+	return (
+		<div>
+			<h1>Ideanick</h1>
+			<div>
+				<div>
+					<h2>Idea 1</h2>
+					<p>This is idea 1</p>
+				</div>
+				<div>
+					<h2>Idea 2</h2>
+          <p>This is idea 2</p>
+				</div>
+			</div>
+		</div>
+	);
+};
